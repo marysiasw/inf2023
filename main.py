9b73599f85d5d0f4ef20b99d5d6fcb71871950f3
marysiasw <137805403+marysiasw@users.noreply.github.com>
2023-07-03 11:52:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,15 +48,6 @@ def show_inst(win: visual.Window, file_name: str, insert: str = 'messages', keys
     win.flip()
     reactions(keys)
 
-    
-
-#def show_text(info, win, keys=None):
- #   if keys is None:
-  #      keys = ["space"]
-   # info.draw()
-    #win.flip()
-    #reactions(keys)
-
 
 def read_text_from_file(file_name: str, insert: str = 'messages'):
     msg = list()
@@ -96,32 +87,8 @@ clock = core.Clock()
 config = load_config()
 print(config)
 
-#inst_tr1 = visual.TextStim(win=window, text=(
-    #"Za chwilę przystąpisz do wykonywania zadania. Polega ono na wciskaniu prawej strzałki, kiedy na ekranie wyświetli się liczba parzysta. Jeżeli podana liczba jest nieparzysta, nie wciskaj nic - zaczekaj na ponowne pojawienie się punktu fiksacji. \n Naciśnij spację by przejść dalej"),
-     #                      height=20)
-#inst_tr12 = visual.TextStim(win=window, text=("Zadanie będzie się składać z 2 bloków, a każdy z trzech serii - przed każdym dostaniesz o nim informacje i możliwość krótkiej przerwy. Postaraj się wykonać zadanie najdokładniej i reaguj najszybciej jak potrafisz. Przed pojawieniem się liczby pojawi się mały krzyżyk na środku ekranu- jest to punkt fiksacji, który ma za zadanie pomóc ci się skupić. Nie spuszczaj go z oczu! \n Naciśnij spację by przejść dalej lub poinformuj osobą prowadzącą, jeżeli masz jakieś wątpliwości"),
-      #                      height=20)
-#inst_tr2 = visual.TextStim(win=window,
-       #                    text="Trening!\n Teraz zacznie się próba treningowa- pamiętaj, reagujesz tylko na liczby parzyste, prawą strzałką na klawiaturze. Naciśnij spację by rozpocząć trening",
-        #                   height=20)
-#inst_exp = visual.TextStim(win=window,
-         #                  text="Trening za tobą! Jeżeli nadal masz jakieś wątpliwości zgłoś to badaczowi, jeśli nie "
-          #                      "możesz zaczynać eksperyment (Uwaga - teraz nie dostaniesz informacji zwrotnej o "
-           #                     "poprawności swoich reakcji) \n Powodzenia! \n Naciśnij spację by rozpocząć eksperyment",
-            #               height=20)
-#inst_break = visual.TextStim(win=window, text="Za tobą pierwszy blok, możesz teraz chwilę odpocząć. "
-             #                                 "Jeżeli jesteś gotowy, aby przejść dalej - naciśnij spację", height=20)
-#inst_end = visual.TextStim(win=window, text="Dziękujemy za udział w eksperymencie", height=20)
 pozytywna_odp = visual.TextStim(win=window, text="DOBRZE!", color='green', height=40)
 negatywna_odp = visual.TextStim(win=window, text="ŹLE!", color='black', height=40)
-#inst_break_fast = visual.TextStim(win=window, text="Jeżeli jesteś gotowy, aby przejść dalej - naciśnij spację",
-              #                    height=20)
-#inst_seria_pierwsza = visual.TextStim(win=window,
-               #                       text="Za tobą pierwsza seria, możesz teraz chwilę odpocząć. Jeżeli jesteś gotowy, aby przejść dalej - naciśnij spację",
-                #                      height=20)
-#inst_seria_kolejna = visual.TextStim(win=window,
-                 #                    text="Za tobą kolejna seria, możesz teraz chwilę odpocząć. Jeżeli jesteś gotowy, aby przejść dalej - naciśnij spację",
-                  #                   height=20)
 fix = visual.TextStim(win=window, text="+", pos=(0, 0), height=40, color=config["FIX_CROSS_COLOR"])
 stim = visual.TextStim(win=window, height=60, color=config['STIM_COLOR2'])
 
@@ -241,7 +208,6 @@ part_of_experiment(n_trials=config["N_TRIALS_EXPERIMENT1"], experiment=True, sz=
 
 # SERIES
 show_inst(window, join('.', 'messages', 'inst_seria_pierwsza.txt'))
-#show_text(info=inst_seria_pierwsza, win=window)
 
 # EXPERIMENT_NORMAL
 part_of_experiment(n_trials=config["N_TRIALS_EXPERIMENT1"], experiment=True, sz=False)
